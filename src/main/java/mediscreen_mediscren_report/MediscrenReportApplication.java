0add453b8e65c79797d3635efc6eb5c798fb6606
Ds_mohamed <smdioubate87@yahoo.com>
2022-07-20 02:44:32 +0200

    initial commit

---
+++ src/main/java/mediscreen_mediscren_report/MediscrenReportApplication.java
@@ -0,0 +1,13 @@
+package mediscreen_mediscren_report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediscrenReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediscrenReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mediscreen_mediscren_report/MediscrenReportApplicationTests.java
@@ -0,0 +1,13 @@
+package mediscreen_mediscren_report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediscrenReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
