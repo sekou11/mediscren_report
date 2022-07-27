57527e95893705e34ea0ca531587eee593c20167
Ds_mohamed <smdioubate87@yahoo.com>
2022-07-28 00:10:46 +0200

    add swagger

--- src/main/java/mediscreen_report/MediscrenReportApplication.java
+++ src/main/java/mediscreen_report/MediscrenReportApplication.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @EnableFeignClients
+
+@EnableSwagger2  
 public class MediscrenReportApplication {
 
 	public static void main(String[] args) {
